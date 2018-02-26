d924f11dc33bb74e3b8b4ff776d766be992e849c
Jeong Juwon <2jw0718@gmail.com>
2018-02-26 16:37:23 +0900

    Update RNIronSourceOfferwallModule.java
    Change event names 
    Add getOfferwallCredits method for running callback user credit event
    Apply to send event on onOfferwallAdCredited event and params added

--- android/src/main/java/co/squaretwo/ironsource/RNIronSourceOfferwallModule.java
+++ android/src/main/java/co/squaretwo/ironsource/RNIronSourceOfferwallModule.java
@@ -4,6 +4,7 @@ import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
 import android.support.annotation.Nullable;
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -33,95 +34,107 @@ public class RNIronSourceOfferwallModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void initializeOfferwall() {
-        new Handler(Looper.getMainLooper()).post(new Runnable() {
-            @Override
-            public void run() {
-                IronSource.setOfferwallListener(new OfferwallListener() {
-                    /**
-                     * Invoked when there is a change in the Offerwall availability status.
-                     * @param - available - value will change to YES when Offerwall are available.
-                     * You can then show the offerwall by calling showOfferwall(). Value will *change to NO when Offerwall isn't available.
-                     */
-                    @Override
-                    public void onOfferwallAvailable(boolean isAvailable) {
-                        Log.d(TAG, "onOfferwallAvailable() called!");
-                        if (isAvailable) {
-                            Log.d(TAG, "ironSourceOfferwall Available!" );
-                            sendEvent("ironSourceOfferwallAvailable", null);
-                        } else {
-                            Log.d(TAG, "ironSourceOfferwall Not Available!" );
-                        }
-                    }
-                    /**
-                     * Invoked when the Offerwall successfully loads for the user, after calling the 'showOfferwall' method
-                     */
-                    @Override
-                    public void onOfferwallOpened() {
-                        Log.d(TAG, "onOfferwallOpened() called!");
-                        sendEvent("ironSourceOfferwallDidShow", null);
-
-                    }
-                    /**
-                     * Invoked when the method 'showOfferWall' is called and the OfferWall fails to load.
-                     * @param error - A IronSourceError Object which represents the reason of 'showOfferwall' failure.
-                     */
-                    @Override
-                    public void onOfferwallShowFailed(IronSourceError error) {
-                        Log.d(TAG, "onOfferwallShowFailed() called!");
-                        sendEvent("ironSourceOfferwallClosedByError", null);
-                    }
-                    /**
-                     * Invoked each time the user completes an Offer.
-                     * Award the user with the credit amount corresponding to the value of the *‘credits’ parameter.
-                     * @param credits - The number of credits the user has earned.
-                     * @param totalCredits - The total number of credits ever earned by the user.
-                     * @param totalCreditsFlag - In some cases, we won’t be able to provide the exact
-                     * amount of credits since the last event (specifically if the user clears
-                     * the app’s data). In this case the ‘credits’ will be equal to the ‘totalCredits’, and this flag will be ‘true’.
-                     * @return boolean - true if you received the callback and rewarded the user, otherwise false.
-                     */
-                    @Override
-                    public boolean onOfferwallAdCredited(int credits, int totalCredits, boolean totalCreditsFlag) {
-                        return false;
-                    }
-                    /**
-                     * Invoked when the method 'getOfferWallCredits' fails to retrieve
-                     * the user's credit balance info.
-                     * @param error - A IronSourceError object which represents the reason of 'getOfferwallCredits' failure.
-                     * If using client-side callbacks to reward users, it is mandatory to return true on this event
-                     */
-                    @Override
-                    public void onGetOfferwallCreditsFailed(IronSourceError error) {
-                    }
-                    /**
-                     * Invoked when the user is about to return to the application after closing
-                     * the Offerwall.
-                     */
-                    @Override
-                    public void onOfferwallClosed() {
-                    }
-                });
+      IronSource.setOfferwallListener(new OfferwallListener() {
+        /**
+          * Invoked when there is a change in the Offerwall availability status.
+          * @param - available - value will change to YES when Offerwall are available.
+          * You can then show the offerwall by calling showOfferwall(). Value will *change to NO when Offerwall isn't available.
+          */
+        @Override
+        public void onOfferwallAvailable(boolean isAvailable) {
+            String eventName = "";
+            if (isAvailable) {
+                eventName = "IronSource-onOfferwallAvailable";
+            } else {
+                eventName = "IronSource-onOfferwallUnavailable";
             }
-        });
+            Log.d(TAG, eventName);
+            sendEvent(eventName, null);
+        }
+        /**
+          * Invoked when the Offerwall successfully loads for the user, after calling the 'showOfferwall' method
+          */
+        @Override
+        public void onOfferwallOpened() {
+            String eventName = "IronSource-onOfferwallOpened";
+            Log.d(TAG, eventName);
+            sendEvent(eventName, null);
+
+        }
+        /**
+          * Invoked when the method 'showOfferWall' is called and the OfferWall fails to load.
+          * @param error - A IronSourceError Object which represents the reason of 'showOfferwall' failure.
+          */
+        @Override
+        public void onOfferwallShowFailed(IronSourceError error) {
+          String eventName = "IronSource-onOfferwallShowFailed";
+          Log.d(TAG, eventName);
+          sendEvent(eventName, null);
+        }
+
+        /**
+          * Invoked each time the user completes an Offer.
+          * Award the user with the credit amount corresponding to the value of the *‘credits’ parameter.
+          * @param credits - The number of credits the user has earned.
+          * @param totalCredits - The total number of credits ever earned by the user.
+          * @param totalCreditsFlag - In some cases, we won’t be able to provide the exact
+          * amount of credits since the last event (specifically if the user clears
+          * the app’s data). In this case the ‘credits’ will be equal to the ‘totalCredits’, and this flag will be ‘true’.
+          * @return boolean - true if you received the callback and rewarded the user, otherwise false.
+          */
+        @Override
+        public boolean onOfferwallAdCredited(int credits, int totalCredits, boolean totalCreditsFlag) {
+          String eventName = "IronSource-onOfferwallAdCredited";
+          Log.d(TAG, eventName);
+
+          WritableMap params = Arguments.createMap();
+          params.putInt("credits", credits);
+          params.putInt("totalCredits", totalCredits);
+          params.putBoolean("totalCreditsFlag", totalCreditsFlag);
+
+          sendEvent(eventName, params);
+          return true;
+        }
+        /**
+          * Invoked when the method 'getOfferWallCredits' fails to retrieve
+          * the user's credit balance info.
+          * @param error - A IronSourceError object which represents the reason of 'getOfferwallCredits' failure.
+          * If using client-side callbacks to reward users, it is mandatory to return true on this event
+          */
+        @Override
+        public void onGetOfferwallCreditsFailed(IronSourceError error) {
+          String eventName = "IronSource-onGetOfferwallCreditsFailed";
+          Log.d(TAG, eventName);
+          sendEvent(eventName, null);
+        }
+        /**
+          * Invoked when the user is about to return to the application after closing
+          * the Offerwall.
+          */
+        @Override
+        public void onOfferwallClosed() {
+          String eventName = "IronSource-onOfferwallClosed";
+          Log.d(TAG, eventName);
+          sendEvent(eventName, null);
+        }
+      });
     }
 
     @ReactMethod
     public void showOfferwall() {
-        new Handler(Looper.getMainLooper()).post(new Runnable() {
-            @Override
-            public void run() {
-                Log.d(TAG, "showOfferwall() called!!");
-                boolean available = IronSource.isOfferwallAvailable();
-                if (available) {
-                    Log.d(TAG, "isOfferwallAvailable() = true");
-                    IronSource.showOfferwall();
-                    sendEvent("ironSourceOfferwallAvailable", null);
-                } else {
-                    Log.d(TAG, "isOfferwallAvailable() = false");
-                    sendEvent("ironSourceOfferwallUnavailable", null);
-                }
-            }
-        });
+      Log.d(TAG, "showOfferwall() called!!");
+      boolean available = IronSource.isOfferwallAvailable();
+      if (available) {
+          Log.d(TAG, "isOfferwallAvailable() = true");
+          IronSource.showOfferwall("DefaultOfferWall");
+      } else {
+          Log.d(TAG, "isOfferwallAvailable() = false");
+      }
+    }
+
+    @ReactMethod
+    public void getOfferwallCredits() {
+      IronSource.getOfferwallCredits();
     }
 
     private void sendEvent(String eventName, @Nullable WritableMap params) {
